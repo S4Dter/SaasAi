e4e528d5cdabf98f373463cf28a0e8846e3e142a
Nico <nico.lfvr@icloud.com>
2025-05-12 14:50:04 +0200

    Mise à jour

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Similarly, allow builds to complete even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Similarly, allow builds to complete even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
+}
+
+export default nextConfig
